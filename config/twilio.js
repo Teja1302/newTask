2393fda34b850df23c1df2a8d88aa6b199f02945
Teja1302 <chittemteja401@gmail.com>
2024-05-17 11:04:14 +0530

    code update

--- config/twilio.js
+++ config/twilio.js
@@ -1,6 +1,6 @@
 
 const twilio = require('twilio');
 
-const twilioClient = twilio('ACbe31901128b13a2a6317c946963cc927', 'a7accd3b309e44c1332719406f432dcc');
+const twilioClient = twilio('process.env.TwiloAccountSID', 'process.env.TwiloAuthToken');
 
 module.exports = twilioClient;
